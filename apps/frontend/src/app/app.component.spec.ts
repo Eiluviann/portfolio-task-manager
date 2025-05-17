bf69e66709fbc4d3403330d12a5810110b1651a1
Sławomir Górski <mickeyhuntr@gmail.com>
2025-05-17 17:38:58 +0200

    Introduced nx

--- apps/frontend/src/app/app.component.spec.ts
+++ apps/frontend/src/app/app.component.spec.ts
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'task-manager' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task-manager');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, task-manager');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, frontend'
+    );
   });
 });
